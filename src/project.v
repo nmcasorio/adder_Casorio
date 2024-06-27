883d8303f5779a2ae3972436cf8362053a3eb55c
nmcasorio <nmcasorio@gmail.com>
2024-06-27 16:21:15 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -26,15 +26,15 @@ module tt_um_example (
     tt_um_adder top(
         .clk(clk),
         .reset(rst_n),
-        .a(ui_in0),
-        .b(ui_in1),
-        .c(ui_in2),
-        .d(ui_in3),
-        .v(uo_out0),
-        .w(uo_out1),
-        .x(uo_out2),
-        .y(uo_out3),
-        .z(uo_out4)
+        .a(ui_in[0]),
+        .b(ui_in[1]),
+        .c(ui_in[2]),
+        .d(ui_in[3]),
+        .v(uo_out[0]),
+        .w(uo_out[1]),
+        .x(uo_out[2]),
+        .y(uo_out[3]),
+        .z(uo_out[4])
     );
 
 endmodule
