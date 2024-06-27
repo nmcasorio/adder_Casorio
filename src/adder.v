b75fc762b305d98614af24aba3076beb0935fd16
nmcasorio <nmcasorio@gmail.com>
2024-06-27 11:25:06 -0500

    Update adder.v

--- src/adder.v
+++ src/adder.v
@@ -1,23 +1,14 @@
-module tt_um_adder (input a, b, c, d output v, w, x, y, z);
+module adder (input a, b, c, d,
+     output v, w, x, y, z);
 
     assign v =  ~a&~b&~c&~d;
 
-    assign w =  ~a&~b&~c&d
-                ~a&~b&c&~d 
-                ~a&b&~c&~d
-                a&~b&~c&~d
+    assign w =  ~a&~b&~c&d | ~a&~b&c&~d | ~a&b&~c&~d | a&~b&~c&~d ;
 
-    assign x =  a&b&~c&~d
-                a&~b&c&~d
-                a&~b&~c&d
-                ~a&b&c&~d
-                ~a&b&~c&d
-                ~a&~b&c&d
+    assign x =  a&b&~c&~d | a&~b&c&~d | a&~b&~c&d | ~a&b&c&~d | ~a&b&~c&dv| ~a&~b&c&d ;
 
-    assign y =  ~a&b&c&d
-                a&~b&c&d
-                a&b&~c&d
-                a&b&c&~d
-    assign z =  a&b&c&d
-    
+    assign y =  ~a&b&c&d | a&~b&c&d | a&b&~c&d | a&b&c&~d ;
+
+    assign z =  a&b&c&d ;
+
 endmodule
