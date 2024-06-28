106c98b469cff616ed8e366ebddfe6f36384ae48
nmcasorio <nmcasorio@gmail.com>
2024-06-28 11:36:42 -0500

    Update adder.v

--- src/adder.v
+++ src/adder.v
@@ -1,26 +1,26 @@
 module tt_um_adder (input clk, 
                     input reset,
                     input ena,
-                    input [0]a, 
-                    input [1]b, 
-                    input [2]c, 
-                    input [3]d,
-                    output [0]v, 
-                    output [1]w, 
-                    output [2]x, 
-                    output [3]y, 
-                    output [4]z
+                    input a, 
+                    input b, 
+                    input c, 
+                    input d,
+                    output v, 
+                    output w, 
+                    output x, 
+                    output y, 
+                    output z
                    );
 
-     assign [0]v =  ~[0]a&~[1]b&~[2]c&~[3]d ;
+     assign v =  ~a&~b&~c&~d ;
 
-     assign [1]w =  ~[0]a&~[1]b&~[2]c&[3]d | ~[0]a&~[1]b&[2]c&~[3]d | ~[0]a&[1]b&~[2]c&~[3]d | [0]a&~[1]b&~[2]c&~[3]d ;
+     assign w =  ~a&~b&~c&d | ~a&~b&c&~d | ~a&b&~c&~d | a&~b&~c&~d ;
 
-     assign [2]x =  [0]a&[1]b&~[2]c&~[3]d | [0]a&~[1]b&[2]c&~[3]d | [0]a&~[1]b&~[2]c&[3]d | ~[0]a&[1]b&[2]c&~[3]d | ~[0]a&[1]b&~[2]c&[3]d| ~[0]a&~[1]b&[2]c&[3]d ;
+     assign x =  a&b&~c&~d | a&~b&c&~d | a&~b&~c&d | ~a&b&c&~d | ~a&b&~c&d| ~a&~b&c&d ;
 
-     assign [3]y =  ~[0]a&[1]b&[2]c&[3]d | [0]a&~[1]b&[2]c&[3]d | [0]a&[1]b&~[2]c&[3]d | [0]a&[1]b&[2]c&~[3]d ;
+  assign y =  ~a&b&c&d | a&~b&c&d | a&b&~c&d | a&b&c&~d ;
 
-     assign [4]z =  [0]a&[1]b&[2]c&[3]d 
+     assign z =  a&b&c&d 
           ;
 
 endmodule
