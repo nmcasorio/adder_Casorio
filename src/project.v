47d127ee3f8b679d614874ebbac79a6dd5acbef5
nmcasorio <nmcasorio@gmail.com>
2024-07-02 10:15:11 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -21,7 +21,7 @@ module tt_um_example (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n,};
 
     tt_um_adder top(
         .clk(clk),
